7e386f1f2a889d29cbb5c5e4ce29d355cff3375e
ksh3490 <ksh349021@gmail.com>
2021-01-12 14:39:22 +0900

    Init commit

---
+++ src/main/java/net/turtle/MyTurtleApplication.java
@@ -0,0 +1,13 @@
+package net.turtle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTurtleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTurtleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/turtle/MyTurtleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.turtle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTurtleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
