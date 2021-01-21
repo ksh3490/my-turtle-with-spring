b9b57e18c9353c744f3a4a8ae0d75d86de0b3960
ksh3490 <ksh349021@gmail.com>
2021-01-21 16:02:41 +0900

    Refactory some URL and prepare to move static html files into templates

--- src/main/java/net/turtle/web/UserController.java
+++ src/main/java/net/turtle/web/UserController.java
@@ -6,27 +6,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.turtle.domain.User;
 import net.turtle.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
 
-	@PostMapping("/createUser")
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user: " + user);
 		userRepository.save(user);
-		return "redirect:/userList";
+		return "redirect:/users";
 	}
 
-	@GetMapping("/userList")
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "userList";
+		return "user/list";
 	}
 
 }
