4e63fc61607d74f52a3a967a9fbd224011ea3a7e
ksh3490 <ksh349021@gmail.com>
2021-01-23 21:46:53 +0900

    Add HiddenHttpMethodFilter to MyTurtleApplication.java

--- src/main/java/net/turtle/MyTurtleApplication.java
+++ src/main/java/net/turtle/MyTurtleApplication.java
@@ -2,12 +2,20 @@ package net.turtle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class MyTurtleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyTurtleApplication.class, args);
+	}
+	
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+		return filter;
 	}
 
 }
